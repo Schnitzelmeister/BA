d31c8f388a0eee7b4eb6268e291d8a9aacd71f4a
Jakub <zvonek.jakub@gmail.com>
2018-08-14 15:23:25 +0200

    Commiting Changes

--- Cheatsheet.py
+++ Cheatsheet.py
@@ -19,3 +19,24 @@ groups = df.groupby('Team')
 #2. Now you have an object of type DataFrameGroupBy containing all the groups
 #3. Extract the group you want, the extracted group is a dataframe
 devils = groups.get_group('Devils')
+
+#Example Query
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+
+titanic[
+    (titanic['sex'] == 'female')
+  & (titanic['class'].isin(['First', 'Third']))
+  & (titanic['age'] > 30)
+  & (titanic['survived'] == 0)
+]
+
+#Convert one particular column to datetime-format
+df['ValueDate'] = pd.to_datetime(df['ValueDate'])
+
+# Set the Date as Index
+df.index = df['ValueDate']
+
+#Truncate Date
+df['starttime'].apply(lambda x: x.date())

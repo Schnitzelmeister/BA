62fdf9477425ed4d521f4bf191cec7c55a741481
Jakub <zvonek.jakub@gmail.com>
2018-08-20 23:46:59 +0200

    Commiting up to date progress

--- Cheatsheets/Cheatsheet.py
+++ Cheatsheets/Cheatsheet.py
@@ -35,8 +35,11 @@ titanic[
   & (titanic['survived'] == 0)
 ]
 
-#Convert one particular column to datetime-format
+#Convert one particular column to datetime-format //NOT possible for STRING
 df['ValueDate'] = pd.to_datetime(df['ValueDate'])
+
+#Convert one particulat column to String
+total_rows['ColumnID'] = total_rows['ColumnID'].astype(str)
 
 # Set the Date as Index
 df.index = df['ValueDate']
@@ -73,4 +76,23 @@ print(math.ceil(4.2))
 #Convert a column in a DF
 df['column'].astype('category')
 
-_________________________MARKDOWN FOR JUPYTER NOTEBOOK_________________________
+#Group by month using a Grouper
+df.groupby(pd.Grouper(freq='M'))
+
+#Group by without using a Grouper
+df.groupby(by=[b.index.month, b.index.year]) #The index most likely has to be a datetimeindex
+
+#Set specific column as index
+df = df.set_index('Datetime') / df.index = df['column']
+
+#Einen Dataframe aus einem JSON File erstellen
+#1. Man öffnet die Datei - passendes Encoding !!!
+#2. Danach konvertiert man das Ganze in einen Dict (Ganz wichtig zu wissen wie man die Elemente im Dict accessed)
+#3. Als letzten Schritt hat man den 
+#Alles außer Nummer ist ein object da Pandas auf Numpy aufgebaut, also beispielsweise auch String ist ein object. Es gibt keinen String Typ in Pandas
+data_file = open('CityBikeWien/Stationendaten.json', encoding='UTF-8') 
+json_dict = json.load(data_file)
+stations_dataframe = pd.DataFrame(json_dict['stations']['station'])
+
+
+
